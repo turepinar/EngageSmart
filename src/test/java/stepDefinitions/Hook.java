5150dfdd923a1668e7b364e2d7a14cd64d8e3258
turepinar <yilmazerp@gmail.com>
2022-05-24 18:00:08 -0500

    commit

--- src/test/java/stepDefinitions/Hook.java
+++ src/test/java/stepDefinitions/Hook.java
@@ -1,5 +1,16 @@
 package stepDefinitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import utils.DriverHelper;
+
 public class Hook {
 
+    WebDriver driver = DriverHelper.getDriver();
+
+    @After
+    public void quitMethod() {
+       DriverHelper.tearDown();
+    }
 }
